d5efa07cf354b3285faab2540b30d5a330a089c7
Kavin0611 <AKPRODUCTION0611@GMAIL.COM>
2025-04-25 14:55:02 +0800

    Setup CI/CD pipeline

---
+++ calculator-app/src/test/java/CalculatorTest.java
@@ -0,0 +1,34 @@
+Set-Content src\test\java\CalculatorTest.java @"
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(8, calculator.add(5, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(15, calculator.multiply(5, 3));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calculator.divide(6, 3));
+    }
+
+    @Test
+    public void testDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> calculator.divide(5, 0));
+    }
+}
+"@
